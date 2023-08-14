a32683db3e18d2e5e5fded74aa0f5755c050521b
henrique2003 <henrique.de.melo.cristioglu@gmail.com>
2023-08-14 19:51:58 -0300

    feat: valdiate fields on create a new realtor

--- web/src/app/cadastrar/page.tsx
+++ web/src/app/cadastrar/page.tsx
@@ -11,11 +11,22 @@ import { Input, InputRadio } from '@/components'
 import validateCpf from '@/utils/validate-cpf'
 
 export default function Register(): JSX.Element {
+  // 1/4
   const [name, setName] = useState('')
   const [surname, setSurname] = useState('')
   const [email, setEmail] = useState('')
   const [celphone, setCelphone] = useState('')
   const [cpf, setCpf] = useState('')
+  // 2/4
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfimation, setPasswordConfimation] = useState('')
+  // 3/4
+  const [trainee, setTrainee] = useState(true)
+  const [supervisorCrecci, setSupervisorCrecci] = useState('')
+  // 4/4
+  const [rg, setRg] = useState<File | null>(null)
+  const [crecci, setCrecci] = useState<File | null>(null)
 
   const onSubmit = async (e: FormEvent): Promise<void> => {
     try {
@@ -32,6 +43,7 @@ export default function Register(): JSX.Element {
       hideProgressBar: false
     }
 
+    // 1/4
     if (!name) {
       return toast.error(deafultErrorMessage('Nome'), toastConfig)
     }
@@ -53,18 +65,68 @@ export default function Register(): JSX.Element {
     if (!cpf || !validateCpf(Number(validCpf))) {
       return toast.error(deafultErrorMessage('Cpf'), toastConfig)
     }
+
+    // 2/4
+    if (!username) {
+      return toast.error(deafultErrorMessage('Nome'), toastConfig)
+    }
+
+    if (!password) {
+      return toast.error(deafultErrorMessage('Senha'), toastConfig)
+    }
+
+    if (password !== passwordConfimation) {
+      return toast.error(deafultErrorMessage('Confirmar senha'), toastConfig)
+    }
+
+    // 3/4
+    if (!trainee) {
+      return toast.error(deafultErrorMessage('Estagiário'), toastConfig)
+    }
+
+    if (!supervisorCrecci) {
+      return toast.error(deafultErrorMessage('Crecci do supervisor'), toastConfig)
+    }
+
+    // 4/4
+    if (!rg) {
+      return toast.error(deafultErrorMessage('Rg'), toastConfig)
+    }
+
+    if (!crecci) {
+      return toast.error(deafultErrorMessage('Crecci'), toastConfig)
+    }
   }
 
   function deafultErrorMessage(field: string): string {
     return `Campo "${field}" inválido`
   }
 
-  const onDrop = useCallback((acceptedFiles: File[]) => {
-    // Do something with the dropped files
-    console.log(acceptedFiles)
+  const onRgDrop = useCallback((acceptedFiles: File[]) => {
+    // Filter acceptedFiles to only keep image and PDF files
+    const validFiles = acceptedFiles.filter(
+      (file) => file.type.startsWith('image/') || file.type === 'application/pdf'
+    )
+
+    setRg(validFiles[0] || null)
   }, [])
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+  const onCrecciDrop = useCallback((acceptedFiles: File[]) => {
+    // Filter acceptedFiles to only keep image and PDF files
+    const validFiles = acceptedFiles.filter(
+      (file) => file.type.startsWith('image/') || file.type === 'application/pdf'
+    )
+
+    setCrecci(validFiles[0] || null)
+  }, [])
+
+  const rgDropzone = useDropzone({
+    onDrop: onRgDrop
+  })
+
+  const crecciDropzone = useDropzone({
+    onDrop: onCrecciDrop
+  })
 
   return (
     <div className="w-full min-h-screen bg-slate-100 flex justify-start items-center px-3 pb-10">
@@ -129,17 +191,23 @@ export default function Register(): JSX.Element {
             <h3 className='text-xl font-semibold text-blue-800 mt-2'>Informação de Acesso</h3>
             <Input
               label='Nome de usuário'
-              name='name'
+              name='username'
+              value={username}
+              onChange={e => setUsername(e.target.value)}
               className='mt-6'
             />
             <Input
               label='Senha'
               name='password'
+              value={password}
+              onChange={e => setPassword(e.target.value)}
               className='mt-4'
             />
             <Input
               label='Confirmar senha'
-              name='passoword_cofirmation'
+              name='passwordConfimation'
+              value={passwordConfimation}
+              onChange={e => setPasswordConfimation(e.target.value)}
               className='mt-4'
             />
           </div>
@@ -153,6 +221,8 @@ export default function Register(): JSX.Element {
               id='trainee'
               className='mt-5'
               value={'true'}
+              defaultChecked
+              onChange={e => setTrainee(Boolean(e.target.value))}
             />
             <InputRadio
               label='Não sou estagiário'
@@ -160,10 +230,13 @@ export default function Register(): JSX.Element {
               id='dontTrainee'
               className='mt-2'
               value={'false'}
+              onChange={e => setTrainee(Boolean(e.target.value))}
             />
             <Input
               label='Creci do supervisor'
               name='supervisioCreci'
+              value={supervisorCrecci}
+              onChange={e => setSupervisorCrecci(e.target.value)}
               className='mt-6'
             />
           </div>
@@ -175,32 +248,34 @@ export default function Register(): JSX.Element {
             <p className='mt-1 text-sm text-gray-500 font-light'>Apenas arquivo .jpg .png ou .pdf. Máximos de 500mb.</p>
             <div
               className={twMerge(
-                'w-full h-[45px] flex items-center justify-center border border-dashed border-blue-900 rounded-[10px] ease duration-300 cursor-pointer mt-5',
-                isDragActive && 'border-green-500'
+                'w-full h-[45px] flex items-center justify-center border border-dashed border-blue-900 rounded-[10px] ease duration-300 cursor-pointer mt-5'
               )}
-              {...getRootProps()}
+              {...rgDropzone.getRootProps()}
             >
-              <input name='rg' {...getInputProps()} />
+              <input {...rgDropzone.getInputProps()} />
               <p className='text-sm text-blue-900 font-medium'>Arraste arquivo aqui ou clique para fazer upload</p>
             </div>
             <div className='flex'>
-              <p className='text-md text-black bg-slate-500 rounded-xl mt-5 px-2 font-normal'>38233.pdf</p>
+              {rg && (
+                <p className='text-md text-black bg-slate-500 rounded-xl mt-5 px-2 font-normal'>{rg.name}</p>
+              )}
             </div>
             <div className='w-full h-[1px] bg-slate-400 my-10'></div>
             <h3 className='text-lg font-bold text-blue-900'>CRECI</h3>
             <p className='mt-1 text-sm text-gray-500 font-light'>Apenas arquivo .jpg .png ou .pdf. Máximos de 500mb.</p>
             <div
               className={twMerge(
-                'w-full h-[45px] flex items-center justify-center border border-dashed border-blue-900 rounded-[10px] ease duration-300 cursor-pointer mt-5',
-                isDragActive && 'border-green-500'
+                'w-full h-[45px] flex items-center justify-center border border-dashed border-blue-900 rounded-[10px] ease duration-300 cursor-pointer mt-5'
               )}
-              {...getRootProps()}
+              {...crecciDropzone.getRootProps()}
             >
-              <input name='creci' {...getInputProps()} />
+              <input itemID='creci' id='creci' name='creci' {...crecciDropzone.getInputProps()} />
               <p className='text-sm text-blue-900 font-medium'>Arraste arquivo aqui ou clique para fazer upload</p>
             </div>
             <div className='flex'>
-              <p className='text-md text-black bg-slate-500 rounded-xl mt-5 px-2 font-normal'>38233.pdf</p>
+              {crecci && (
+                <p className='text-md text-black bg-slate-500 rounded-xl mt-5 px-2 font-normal'>{crecci.name}</p>
+              )}
             </div>
           </div>
           <button type='submit' className='w-[100px] h-[40px] bg-blue-900 ease duration-300 hover:bg-blue-800 text-white font-bold text-md rounded-[8px] mt-5'>Criar</button>
